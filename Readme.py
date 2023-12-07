6403b0a501e471f989f95efc53731e927bd79277
Nazareno <nazareno@.com>
2023-12-06 23:42:29 -0300

    commit

---
+++ Readme.py
@@ -0,0 +1,34 @@
+""""
+
+*Funcionalidades
+Página Principal
+Ruta: http://127.0.0.1:8000/
+Descripción: Página de inicio con enlaces a las secciones de formularios.
+
+Agregar Categoría
+Ruta: http://127.0.0.1:8000/categoria_form/
+Descripción: Formulario para agregar una nueva categoría.
+
+Agregar Producto
+Ruta: http://127.0.0.1:8000/producto_form/
+Descripción: Formulario para agregar un nuevo producto, con la capacidad de seleccionar la categoría desde una lista.
+
+Agregar Cliente
+Ruta: http://127.0.0.1:8000/cliente_form/
+Descripción: Formulario para agregar un nuevo cliente.
+
+Buscar Productos
+Ruta: http://127.0.0.1:8000/search_productos/
+Descripción: Formulario de búsqueda que filtra los productos por categoría.
+
+*Cómo Probar
+
+Accede a la Página Principal y navega a las diferentes secciones.
+
+Utiliza los formularios para agregar nuevas categorías, productos y clientes.
+Explora la funcionalidad de búsqueda en la página Buscar Productos.
+Accede a la Página Principal y navega a las diferentes secciones.
+Utiliza los formularios para agregar nuevas categorías, productos y clientes.
+Explora la funcionalidad de búsqueda en la página Buscar Productos.
+
+"""
